6e97deaedff3dcb573e03e747ef1f6bd1f14a278
Harshita Middha <145043751+Harshitamiddha05@users.noreply.github.com>
2026-01-16 10:51:58 +0530

    Add files via upload

---
+++ leetode_231.cpp
@@ -0,0 +1,12 @@
+#include<math.h>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)
+          return false;
+        double k=log2(n);
+        if(floor(k)==k)
+           return true;
+        return false;
+    }
+};
