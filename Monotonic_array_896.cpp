d45aefea8f29eaf1a4b21a4362cfd25aaecc9b4f
Harshita Middha <145043751+Harshitamiddha05@users.noreply.github.com>
2026-01-21 00:01:21 +0530

    Add files via upload

---
+++ Monotonic_array_896.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+class Solution {
+    public :
+      bool isMonotonic(vector <int>& nums) {
+        bool flag=false;
+        bool flag1=false;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1])
+               flag=true;
+            if(nums[i]>nums[i-1])
+               flag1=true;
+            if(flag && flag1)
+               return false;
+        }
+        return true;
+    }
+};
